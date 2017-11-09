5ae6700e76a598557986eccc8bf1c8093e21a4f8
Oliver Steele <steele@osteele.com>
2017-11-09 09:57:16 -0500

    Add firebase, markdown, svg

--- src/App.js
+++ src/App.js
@@ -1,57 +1,77 @@
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 import logo from './logo.svg';
+import sightingsLogo from './sightings.svg';
 
-var text = `
-There’s no substitute for the experience of working with a live client - real schedules, real feedback, real politics, real changes, etc.…
-
-1. STUDENT PRESENTATION
-In order to present the work to clients students are forced to put words to the work that they have done in a way that allows an outside audience to understand whats being shown.
-
-2. CLIENT RESPONSE
-Instant feedback, both through body language and verbal response, exposes students to authentic critique which will allow them to deliver a stronger second round of designs.
-This interaction also “ups the ante” for students and creates a motivation for delivering appropriate solutions for their specific audience.
-
-3. PROFESSOR INPUT
-At the end of the meeting the professor takes the opportunity to add in anything that the student may have missed.
-This serves as instant feedback for the student on then quality of their presentation and creates the feeling of presenting as a team rather than the student feeling like the success of failure of the presentation rests only on them.
-
->> READ MORE
-NARRATED BY: Tim Ferguson Sauder
-OTHERS INVOLVED: Annabel Consilvio, Kristen O’Neil
-`
+var config = {
+  apiKey: "AIzaSyDW_PeMTIDfRwff2vev-PzQS2R1IV9_ZN0",
+  authDomain: "olin-odes.firebaseapp.com",
+  projectId: "olin-odes",
+};
+var firebase = window.firebase;
+firebase.initializeApp(config);
+var db = firebase.firestore();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { sightings: [] };
+  }
+
+  componentWillMount() {
+    db.collection('sightings').get().then((qs) => {
+      let sightings = [];
+      qs.forEach((ds) => {
+        let s = ds.data()
+        s.key = ds.id;
+        sightings.push(s)
+      })
+      this.setState({ sightings })
+    }).catch(console.error);
+  }
+
   render() {
     return (
-      <div class="App">
+      <div className="App">
         <header>
           <img src={logo} alt="logo" />
           <div>Design Elements</div>
         </header>
         <h1>Foster Student Autonomy</h1>
-        <p class="desc">
+        <p className="desc">
           “Student development of self-directed learning skills is critical for success in today’s rapidly-changing engineering world. The details of how instructors may best foster engagement in life-long learning, however, are unclear; many educators have struggled to define, implement, and assess lifelong learning in engineering curricula.”
           – JON STOLK
         </p>
-        <div class="sightings">
+
+        <div className="sightings">
           <header>
-            <h2>Sightings</h2>
+            <h2><img src={sightingsLogo} alt="logo" />Sightings</h2>
           </header>
-          <section class="sighting">
-            <h3 style={{ background: 'url(logox.svg)' }}>Student-Run Meetings</h3>
-            <dl>
-              <dt>Professor</dt>
-              <dd>Tim Ferguson Sauder</dd>
-              <dt>Class</dt>
-              <dd>Return Design Studio Practicum</dd>
-            </dl>
-            {text}
-          </section>
+          {this.state.sightings.map(Sighting)}
         </div>
       </div>
     );
   }
+}
+
+function Sighting(props) {
+  return <section className="sighting" key={props.key}>
+    <h3 style={{ background: 'url(poster.png)' }}>{props.topic}</h3>
+    <dl>
+      <dt>Professor</dt>
+      <dd>{props.instructor}</dd>
+      <dt>Class</dt>
+      <dd>{props.course}</dd>
+    </dl>
+    <ReactMarkdown source={props.description.replace(/\\n/g, '\n')} />
+    <dl>
+      <dt>Narrated by</dt>
+      <dd>{props.narrators}</dd>
+      <dt>Others involved</dt>
+      <dd>{props.participants}</dd>
+    </dl>
+  </section>
 }
 
 export default App;
