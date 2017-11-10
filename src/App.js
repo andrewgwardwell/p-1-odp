f97253cddc0fe532512f543edef0d4addcd54754
Oliver Steele <steele@osteele.com>
2017-11-09 23:48:34 -0500

    Read poster URL from store

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import ReactMarkdown from 'react-markdown';
 import './App.css';
 import logo from './logo.svg';
 
-var config = {
+let config = {
   apiKey: "AIzaSyDW_PeMTIDfRwff2vev-PzQS2R1IV9_ZN0",
   authDomain: "olin-odes.firebaseapp.com",
   projectId: "olin-odes",
+  storageBucket: "olin-odes.appspot.com",
 };
-var firebase = window.firebase;
+let firebase = window.firebase;
 firebase.initializeApp(config);
-var db = firebase.firestore();
+let db = firebase.firestore();
+let storage = firebase.storage();
+let storageRef = storage.ref();
 
 class App extends Component {
   render() {
@@ -57,7 +60,7 @@ class ODE extends Component {
             <h2>Sightings</h2>
           </header>
           <section className="sightings">
-            {this.state.sightings.map(Sighting)}
+            {this.state.sightings.map((x) => <Sighting key={x.key} {...x} />)}
           </section>
         </div>
       </div>
@@ -65,23 +68,40 @@ class ODE extends Component {
   }
 }
 
-function Sighting(props) {
-  return <section className="sighting" key={props.key}>
-    <h3 style={{ background: 'url(poster.png)' }}>{props.title}</h3>
-    <dl>
-      <dt>Professor</dt>
-      <dd>{props.instructors}</dd>
-      <dt>Class</dt>
-      <dd>{props.course}</dd>
-    </dl>
-    <ReactMarkdown source={props.description.replace(/\\n/g, '\n')} />
-    <dl>
-      <dt>Narrated by</dt>
-      <dd>{props.narrators}</dd>
-      <dt>Others involved</dt>
-      <dd>{props.participants}</dd>
-    </dl>
-  </section>
+class Sighting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { background: 'blue' };
+  }
+
+  componentWillMount() {
+    if (this.props.poster) {
+      let posterRef = storageRef.child(this.props.poster);
+      posterRef.getDownloadURL().then((poster_url) => {
+        this.setState({ background: `url(${poster_url})` });
+      }).catch(console.error);
+    }
+  }
+
+  render() {
+    let props = this.props;
+    return <section className="sighting" key={props.key}>
+      <h3 style={{ background: this.state.background }}>{props.title}</h3>
+      <dl>
+        <dt>Professor</dt>
+        <dd>{props.instructors}</dd>
+        <dt>Class</dt>
+        <dd>{props.course}</dd>
+      </dl>
+      <ReactMarkdown source={props.description.replace(/\\n/g, '\n')} />
+      <dl>
+        <dt>Narrated by</dt>
+        <dd>{props.narrators}</dd>
+        <dt>Others involved</dt>
+        <dd>{props.participants}</dd>
+      </dl>
+    </section>
+  }
 }
 
 export default App;
